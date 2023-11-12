998e37b49ab3d1f11f9fb1a042413b2091e25931
MarcoAAlmeida <marcoalmeida.dev.br@gmail.com>
2023-11-12 18:16:45 -0300

    enabled actuator management configurations

---
+++ src/main/java/br/dev/marcoalmeida/mb/DevProfileSecurityConfiguration.java
@@ -0,0 +1,28 @@
+package br.dev.marcoalmeida.mb;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("dev")
+@Configuration(proxyBeanMethods = false)
+public class DevProfileSecurityConfiguration {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher(PathRequest.toH2Console());
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers((headers) -> headers.frameOptions((frame) -> frame.sameOrigin()));
+        return http.build();
+    }
+
+
+}
---
+++ src/main/java/br/dev/marcoalmeida/mb/SecurityConfiguration.java
@@ -0,0 +1,60 @@
+package br.dev.marcoalmeida.mb;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                newUser("player1"),
+                newUser("player2"),
+                newUser("player3"));
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain managementFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private UserDetails newUser(String username) {
+        return User.builder()
+                .username(username)
+                .password(passwordEncoder().encode(username))
+                .roles("USER")
+                .build();
+    }
+
+}
