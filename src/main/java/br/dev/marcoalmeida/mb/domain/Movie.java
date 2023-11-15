6cd579d584b6b73165db7bcfd19f9ab33069f921
MarcoAAlmeida <marcoalmeida.dev.br@gmail.com>
2023-11-14 21:29:28 -0300

    excluded Movie properties from equals()

---
+++ src/main/java/br/dev/marcoalmeida/mb/domain/Movie.java
@@ -0,0 +1,23 @@
+package br.dev.marcoalmeida.mb.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Movie {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String title;
+
+
+
+}
---
+++ src/test/java/br/dev/marcoalmeida/mb/domain/MovieTests.java
@@ -0,0 +1,57 @@
+package br.dev.marcoalmeida.mb.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class MovieTests {
+
+    @Test
+    public void WhemEmptyConstructor_NullifiedProperties(){
+        Movie m = new Movie();
+        assertThat(m.getId()).isNull();
+    }
+
+    @Test
+    public void WhenEmptyBuilder_NullifiedProperties(){
+        Movie m = Movie.builder().build();
+        assertThat(m.getId()).isNull();
+    }
+
+
+    @Test
+    public void WhenNonEmptyBuilder_NonNullProperties(){
+        Movie m = Movie.builder()
+                .id(1L)
+                .title("testMovie")
+                .build();
+        assertThat(m.getId()).isNotNull();
+        assertThat(m.getTitle()).isNotNull();
+    }
+
+    @Test
+    public void WhenStaticBuilder_NonNullProperties(){
+        Movie m = Movie.of(1L, "testMovie");
+        assertThat(m.getId()).isNotNull();
+        assertThat(m.getTitle()).isNotNull();
+    }
+
+    @Test
+    public void WhenSameIdentity_ObjectsAreEqual(){
+        Movie m1 = Movie.builder()
+                .id(1L)
+                .title("one")
+                .build();
+
+        Movie m2 = Movie.builder()
+                .id(1L)
+                .title("two")
+                .build();
+
+        assertThat(m1).isEqualTo(m2);
+    }
+
+}
