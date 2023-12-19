e549ee986332d8378b4efbd14536656e5071df92
williamkotesky <103445326+williamkotesky@users.noreply.github.com>
2023-12-19 20:00:24 -0300

    add unit tests (#24)

--- src/test/java/br/dev/marcoalmeida/mb/controller/PlayerControllerTests.java
+++ src/test/java/br/dev/marcoalmeida/mb/controller/PlayerControllerTests.java
@@ -1,16 +1,34 @@
 package br.dev.marcoalmeida.mb.controller;
 
 import br.dev.marcoalmeida.mb.MbAbstractTest;
+import br.dev.marcoalmeida.mb.dto.UnfinishedGameByPlayerDTO;
+import br.dev.marcoalmeida.mb.repository.GameRepository;
+import br.dev.marcoalmeida.mb.repository.RoundRepository;
+import br.dev.marcoalmeida.mb.service.PlayerService;
 import br.dev.marcoalmeida.mb.service.RoundService;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
 
 @ExtendWith(MockitoExtension.class)
 public class PlayerControllerTests extends MbAbstractTest {
 
+	@Mock
+	private PlayerService playerService;
+	
+	@Mock
+	private GameRepository gameRepository;
+	
+	@Mock
+	private RoundRepository roundRepository;
+	
     @Mock
     private RoundService roundService;
 
@@ -19,7 +37,14 @@ public class PlayerControllerTests extends MbAbstractTest {
 
     @Test
     public void WhenUnfinishedGameExists_UnfinishedGameByPlayerDTOReturned() {
-        // TODO
+    	
+    	when(playerService.getUnfinishedGameByPlayer(PLAYER1_ID))
+			.thenReturn(UNFINISHED_GAME_BY_PLAYER_DTO);
+    	
+    	ResponseEntity<UnfinishedGameByPlayerDTO> responseEntity = playerController.getUnfinishedGameByPlayer(PLAYER1_ID);
+ 	  	
+    	assertThat(responseEntity.getBody()).isInstanceOf(UnfinishedGameByPlayerDTO.class);
+    	assertThat(responseEntity.getBody()).isEqualTo(UNFINISHED_GAME_BY_PLAYER_DTO);    	
     }
 
 }
--- src/test/java/br/dev/marcoalmeida/mb/service/PlayerServiceTest.java
+++ src/test/java/br/dev/marcoalmeida/mb/service/PlayerServiceTest.java
@@ -1,8 +1,17 @@
 package br.dev.marcoalmeida.mb.service;
 
 import br.dev.marcoalmeida.mb.MbAbstractTest;
+import br.dev.marcoalmeida.mb.dto.UnfinishedGameByPlayerDTO;
+import br.dev.marcoalmeida.mb.exception.MbException;
 import br.dev.marcoalmeida.mb.repository.GameRepository;
 import br.dev.marcoalmeida.mb.repository.RoundRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,7 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class PlayerServiceTest extends MbAbstractTest {
-    @Mock
+	@Mock
     private GameRepository gameRepository;
 
     @Mock
@@ -20,19 +29,43 @@ public class PlayerServiceTest extends MbAbstractTest {
 
     @InjectMocks
     private PlayerService playerService;
+    
+    private static final String UNFINISHED_GAME_NOT_FOUND = "could not find unfinished Game for playerId [%s]";
 
     @Test
     public void WhenUnfinishedGameExists_UnfinishedGameByPlayerDTOReturned(){
-        // TODO
+    	
+    	when(gameRepository.findByPlayer_IdAndFinishedAtIsNull(PLAYER1_ID))
+        	.thenReturn(Optional.of(GAME1));
+    	when(roundRepository.findByGame_IdAndAnsweredAtIsNull(GAME1_ID))
+    		.thenReturn(Optional.of(ROUND1));
+    	
+    	UnfinishedGameByPlayerDTO unfinishedGameByPlayerDTO = playerService.getUnfinishedGameByPlayer(PLAYER1_ID);
+    	
+    	assertThat(unfinishedGameByPlayerDTO).isNotNull();
+    	assertThat(unfinishedGameByPlayerDTO.getGameId()).isEqualTo(GAME1.getId());   	
     }
 
     @Test
     public void WhenUnfinishedGameDoesNotExist_MbExceptionIsThrown(){
-        // TODO
+    	
+    	when(gameRepository.findByPlayer_IdAndFinishedAtIsNull(PLAYER1_ID))
+    		.thenReturn(Optional.empty());
+    	
+    	assertThatThrownBy(() -> playerService.getUnfinishedGameByPlayer(PLAYER1_ID))
+        	.isInstanceOf(MbException.class)
+        	.hasMessage(String.format(UNFINISHED_GAME_NOT_FOUND, PLAYER1_ID));	
     }
 
+    @Test
     public void WhenPlayerIdDoesNotExist_MbExceptionIsThrown(){
-        // TODO
+    	
+    	when(gameRepository.findByPlayer_IdAndFinishedAtIsNull(PLAYER1_ID))
+			.thenReturn(Optional.empty());
+	
+    	assertThatThrownBy(() -> playerService.getUnfinishedGameByPlayer(PLAYER1_ID))
+    		.isInstanceOf(MbException.class)
+    		.hasMessage(String.format(UNFINISHED_GAME_NOT_FOUND, PLAYER1_ID));	
     }
 
 
